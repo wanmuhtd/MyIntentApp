37190af024e0f86dabddb4f5f913a95fc8b3ba21
wanmuhtd <ryskrnzo@gmail.com>
2024-09-30 16:15:51 +0700

    Adding new activity MoveActivity.kt & activity_move.xml

--- app/src/main/java/com/dicoding/myintentapp/MoveActivity.kt
+++ app/src/main/java/com/dicoding/myintentapp/MoveActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+//ini MoveActivity
 class MoveActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
